20b75c37fe66caa2be83ce4cbfb79153187ee28b
Laura Bassani <bassanilaurarita@gmail.com>
2021-04-10 18:35:28 +0200

    user can submit relative scores

--- src/Scores.java
+++ src/Scores.java
@@ -4,8 +4,35 @@ public class Scores {
 
     Hashtable<String, Integer> scores_dict = new Hashtable<String, Integer>();
 
-    public void setUserScore(String user, int points) {
-        scores_dict.put(user,points);
+    public void setUserScore(String user, String points) {
+        if (userExists(user)) {
+            int actualUserPoints = scores_dict.get(user);
+            int num = Math.abs(Integer.parseInt(points));
+            if (isSubstraction(points)) {
+                int score = actualUserPoints - num;
+                scores_dict.put(user, score);
+            } else if (isSum(points)) {
+                int score = actualUserPoints + num;
+                scores_dict.put(user, score);
+            } else {
+                scores_dict.put(user, num);
+            }
+        } else {
+            scores_dict.put(user, Integer.parseInt(points));
+        }
+
+    }
+
+    private boolean isSum(String points) {
+        return Character.compare(points.charAt(0), '+') == 0;
+    }
+
+    private boolean isSubstraction(String points) {
+        return Character.compare(points.charAt(0), '-') == 0;
+    }
+
+    private boolean userExists(String user) {
+        return scores_dict.containsKey(user);
     }
 
     public int getAbsolutScores(String user) {
--- tests/ScoresTest.java
+++ tests/ScoresTest.java
@@ -8,9 +8,18 @@ class ScoresTest {
     @Test
     void shouldSaveUserTotalScore() {
         Scores s = new Scores();
-        s.setUserScore("123", 120);
-        s.setUserScore("124", 150);
+        s.setUserScore("123", "120");
+        s.setUserScore("124", "150");
         Assertions.assertEquals(120, s.getAbsolutScores("123"));
         Assertions.assertEquals(150, s.getAbsolutScores("124"));
+    }
+
+    @Test
+    void shouldSaveUserRelativeScore() {
+        Scores s = new Scores();
+        s.setUserScore("123", "120");
+        s.setUserScore("123", "-20");
+        s.setUserScore("123", "+30");
+        Assertions.assertEquals(130, s.getAbsolutScores("123"));
     }
 }
